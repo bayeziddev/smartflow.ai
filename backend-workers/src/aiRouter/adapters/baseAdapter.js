a7826636ed17bec70f3198ae55481c47f92f1987
Sayad Bayezid <sayadmdbayezidhosan@gmail.com>
2026-07-29 18:18:16 +0600

    Delete backend-workers directory git rm -rf .
    git rm -rf .
    
    Signed-off-by: Sayad Bayezid <sayadmdbayezidhosan@gmail.com>

--- backend-workers/src/aiRouter/adapters/baseAdapter.js
+++
@@ -1,7 +0,0 @@
-export class ProviderError extends Error {
-  constructor(message, status, providerRawError = null) {
-    super(message);
-    this.status = status || 500;
-    this.providerRawError = providerRawError;
-  }
-}
--- backend-workers/src/httpError.js
+++
@@ -1,7 +0,0 @@
-export class HttpError extends Error {
-  constructor(message, statusCode = 500, code = 'INTERNAL_ERROR') {
-    super(message);
-    this.statusCode = statusCode;
-    this.code = code;
-  }
-}
